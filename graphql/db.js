c6c2977e9565bcbae1744bab7cc0726e4c6680eb
Dongeun Paeng <dongeun.paeng@gmail.com>
2020-07-25 21:28:55 +0900

    Modified according to the client-side JS

--- graphql/db.js
+++ graphql/db.js
@@ -1,5 +1,7 @@
 import fetch from "node-fetch";
-const API_URL = "https://yts.am/api/v2/list_movies.json?";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+const GET_MOVIE_URL = "https://yts.mx/api/v2/movie_details.json?";
+const GET_SUGGESTION_URL = "https://yts.mx/api/v2/movie_suggestions.json?";
 
 export const getMovies = (limit, rating) => {
   let REQUEST_URL = API_URL;
@@ -7,7 +9,27 @@ export const getMovies = (limit, rating) => {
     REQUEST_URL += `limit=${limit}`;
   }
   if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`;
+    REQUEST_URL += `rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
+};
+
+export const getMovie = (id) => {
+  let REQUEST_URL = GET_MOVIE_URL;
+  if (id > 0) {
+    REQUEST_URL += `movie_id=${id}`;
+  }
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movie);
+};
+
+export const getSuggestions = (id) => {
+  let REQUEST_URL = GET_SUGGESTION_URL;
+  if (id > 0) {
+    REQUEST_URL += `movie_id=${id}`;
   }
   return fetch(REQUEST_URL)
     .then((res) => res.json())
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,11 @@
 // graphql 파일은 description이고, 실제로 일을 수행하는 것은 resolver.js이다.
-import { getMovies } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
 
